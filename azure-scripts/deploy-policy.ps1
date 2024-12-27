2a447b5bdde65410ac9dce5b8bd781c2c4ed12d1
nkpotthuru80 <nkpotthuru80@outlook.com>
2024-12-27 22:07:43 +0530

    depth increased

--- azure-scripts/deploy-policy.ps1
+++ azure-scripts/deploy-policy.ps1
@@ -1,11 +1,11 @@
  # Import policy definition JSON from file
  $policyContent = Get-Content -Path './builtin-policy.json' -Raw
  $policyContent = $policyContent.Trim()
- $policyDefinition = $policyContent | ConvertFrom-Json -Depth 10 
+ $policyDefinition = $policyContent | ConvertFrom-Json -Depth 100 
  
  # Access specific properties and convert them to strings
  $displayName = $policyDefinition.properties.displayName.ToString()
- $policyRule = $policyDefinition.properties.policyRule | ConvertTo-Json -Compress -Depth 10
+ $policyRule = $policyDefinition.properties.policyRule | ConvertTo-Json -Compress -Depth 100
  $description = $policyDefinition.properties.description.ToString()
  
  # Create or update the policy definition
